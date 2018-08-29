31e08e34c7d9bc967ec788fc533b89dbcac7e12d
yunwen <shenyunwen520@qq.com>
2018-08-29 23:23:38 +0800

    工具集合

--- build.py
+++ build.py
@@ -1,10 +1,4 @@
 # -*-coding: utf-8 -*-
-"""
-1、从svn上更新vue前端项目
-2、打包vue前端包
-3、上传到前端服务器
-4、重启nginx
-"""
 import os
 import datetime
 import paramiko
@@ -13,58 +7,19 @@ import paramiko
 PROJECT_DIR = r"E:\ERP\vueProject"
 # 本地存放前端dist包的目录
 LOCAL_DIST_PATH = r"E:\ERP\vueProject\dist"
+# 本地存放前端dist包的父目录
+LOCAL_DIST_PARENT_PATH = "\\".join(LOCAL_DIST_PATH.split("\\")[0:-1])
+# 本地存放前端dist包的相对路径
+LOCAL_DIST_RELATIVE_PATH = "\\".join(LOCAL_DIST_PATH.split("\\")[0:-1]) + "\\"
 # 远程服务器存放dist包的目录
 REMOTE_DIST_PATH = '/home/nginx/nginx-setup/html/dist'
+# 远程服务器存放dist包的父目录
+REMOTE_DIST_PARENT_PATH = "/".join(REMOTE_DIST_PATH.split("/")[0:-1])
 
 HOST_NAME = '192.168.1.225'
 USER_NAME = 'root'
 PASSWORD = 'dj123456'
 PORT = 22
-
-
-class UploadFile:
-    def __init__(self, hostname, username, password, localPath, remotePath, port=22):
-        print("正在与服务器连接")
-        self.hostname = hostname
-        self.username = username
-        self.password = password
-        self.port = port
-        self.localPath = localPath
-        self.localParentPath = "\\".join(localPath.split('\\')[0:-1])
-        self.remotePath = remotePath
-        self.remoteParentPath = "/".join(remotePath.split('/')[0:-1])
-        # sftp
-        self.transport = paramiko.Transport(self.hostname, self.port)
-        self.transport.connect(username=username, password=password)
-        self.sftp = paramiko.SFTPClient.from_transport(self.transport)
-        # ssh
-        self.ssh_client = paramiko.SSHClient()
-        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ssh_client.connect(self.hostname, self.port, username, password)
-
-    def upload(self):
-        try:
-            for root, dirs, files in os.walk(self.localPath):
-                newRoot = self.remoteParentPath + "/" + root.replace("E:\\ERP\\vueProject\\", "")
-                for file in files:
-                    localPath = os.path.join(root, file)
-                    remotePath = os.path.join(newRoot, file).replace('\\', '/')
-                    print("[INFO]原文件地址", localPath)
-                    print("[INFO]新文件地址", remotePath)
-                    print("---" * 20)
-
-                    self.sftp.put(localPath, remotePath)
-        except Exception as e:
-            print(e)
-
-    def reset_nginx(self):
-        cmd = "cd /home/nginx/nginx-setup/sbin;"
-        self.ssh_client.exec_command(cmd)
-        cmd = "./nginx -s stop"
-        self.ssh_client.exec_command(cmd)
-        cmd = "./nginx"
-        self.ssh_client.exec_command(cmd)
-        print("[INFO]正在重启nginx服务")
 
 
 def update_code():
@@ -73,8 +28,8 @@ def update_code():
     """
     print("[INFO]正在更新代码")
     cmd = "svn update " + PROJECT_DIR
-    p1 = os.system(cmd)
-    raise_error(p1, "更新代码错误")
+    if os.system(cmd) != 0:
+        raise RuntimeError("更新代码失败")
     print("[INFO]正在更新代码成功")
 
 
@@ -86,37 +41,93 @@ def build_project():
     os.chdir("E:")
     os.chdir("E:/ERP/vueProject")
     build_cmd = "npm run build"
-    p3 = os.system(build_cmd)
-    raise_error(p3, "打包失败")
+    if os.system(build_cmd) != 0:
+        raise RuntimeError("打包失败")
     print("[INFO]正在打包前端包成功")
 
 
-def upload_to_server():
+def login_sftp(hostname, username, password, port=22):
     """
-    上传到前端服务器重启服务
+    sftp登录
+    :param hostname: 主机名
+    :param username: 用户名
+    :param password: 密码
+    :param port: 端口
+    :return: sftp
     """
-    uploadFile = UploadFile(HOST_NAME, USER_NAME, PASSWORD, LOCAL_DIST_PATH, REMOTE_DIST_PATH)
-    uploadFile.upload()
-    uploadFile.reset_nginx()
+    transport = paramiko.Transport(hostname, port)
+    transport.connect(username=username, password=password)
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    return sftp
 
 
-def raise_error(code, reason):
+def login_ssh(hostname, username, password, port=22):
     """
-    抛出异常
-    :param code: 运行结果
-    :param reason: 抛出原因
+    ssh登录
+    :param hostname: 主机名
+    :param username: 用户名
+    :param password: 密码
+    :param port: 端口
+    :return:
     """
-    if code != 0:
-        raise RuntimeError(reason)
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, port, username, password)
+    return ssh
 
 
-def main():
+def upload(sftp, local_path):
+    """
+    上传dist包到服务器
+    :param sftp: sftp句柄
+    :param local_path: 本地dist包目录
+    :return:
+    """
+    try:
+        for root, dirs, files in os.walk(local_path):
+            dist_root = REMOTE_DIST_PARENT_PATH + "/" + root.replace(LOCAL_DIST_RELATIVE_PATH, "")
+            for file in files:
+                local_path = os.path.join(root, file)
+                remote_path = os.path.join(dist_root, file).replace('\\', '/')
+                print("[INFO]原文件地址", local_path)
+                print("[INFO]新文件地址", remote_path)
+                print("---" * 20)
+                sftp.put(local_path, remote_path)
+    except Exception as e:
+        print(e)
+
+
+def reset_nginx(ssh):
+    """
+    重启nginx服务器
+    :param ssh:
+    :return:
+    """
+    cmd = "cd /home/nginx/nginx-setup/sbin;"
+    ssh.exec_command(cmd)
+    cmd = "./nginx -s stop"
+    ssh.exec_command(cmd)
+    cmd = "./nginx"
+    ssh.exec_command(cmd)
+    print("[INFO]正在重启nginx服务")
+
+
+def run():
+    """
+    1、从svn上更新vue前端项目
+    2、打包vue前端包
+    3、上传到前端服务器
+    4、重启nginx
+    """
     update_code()
     build_project()
-    upload_to_server()
+    sftp = login_sftp(HOST_NAME, USER_NAME, PASSWORD)
+    upload(sftp, LOCAL_DIST_PATH)
+    ssh = login_ssh(HOST_NAME, USER_NAME, PASSWORD)
+    reset_nginx(ssh)
 
 
 if __name__ == "__main__":
     print("start...", datetime.datetime.now())
-    main()
+    run()
     print("end....", datetime.datetime.now())
